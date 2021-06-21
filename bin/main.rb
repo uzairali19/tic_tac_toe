0b093f503d11da890889a11734ca1012e2e867d7
Uzair <uzairali19b@gmail.com>
2021-06-21 21:05:38 +0500

    update linter error fix

--- bin/main.rb
+++ bin/main.rb
@@ -1,12 +1,12 @@
-require_relative "../lib/board"
-require_relative "../lib/player"
+require_relative '../lib/board'
+require_relative '../lib/player'
 
 puts "Welcome to Ruby's Tic-Tac-Toe!"
 puts
-puts "Enter Player 1 Name:"
+puts 'Enter Player 1 Name:'
 player1 = gets.chomp
 puts
-puts "Enter Player 2 Name:"
+puts 'Enter Player 2 Name:'
 player2 = gets.chomp
 puts
 puts "#{player1} will play with X and #{player2} will play with O"
