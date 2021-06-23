670069b52d6e12d2e4960c81c3c78599ae68047f
Uzair <uzairali19b@gmail.com>
2021-06-23 21:19:19 +0500

    update main.rb input filters

--- bin/main.rb
+++ bin/main.rb
@@ -9,11 +9,31 @@ end
 
 puts "Welcome to Ruby's Tic-Tac-Toe!"
 puts
-puts 'Enter Player 1 Name:'
+puts "Enter Player 1 Name:"
 player1 = gets.chomp
+if player1.empty?
+  puts "Name field cannot be empty"
+  player1 = gets.chomp
+elsif player1.length < 3
+  puts "Name cannot be less than 3 letters"
+  player1 = gets.chomp
+elsif player1.to_i.to_s.eql? player1
+  puts "Only text is allowed"
+  player1 = gets.chomp
+end
 puts
-puts 'Enter Player 2 Name:'
+puts "Enter Player 2 Name:"
 player2 = gets.chomp
+if player2.empty?
+  puts "Name field cannot be empty"
+  player2 = gets.chomp
+elsif player2.length < 3
+  puts "Name cannot be less than 3 letters"
+  player2 = gets.chomp
+elsif player2.to_i.to_s.eql? player2
+  puts "Only text is allowed"
+  player2 = gets.chomp
+end
 puts
 puts "#{player1} will play with X and #{player2} will play with O"
 puts
@@ -30,52 +50,36 @@ puts board_layout = " +---+---+---+
  +---+---+---+"
 
 puts "It's #{player1}'s turn"
-puts 'Please select an available number from the board'
+puts "Please select an available number from the board"
 puts
 num = gets.chomp.to_i
 puts
-if num <= 9 || num >= 1
-  num
-else
-  'Invalid entry'
+until board.include?(num)
+  puts "invalid move. Please enter a number from 1-9"
+  num = gets.chomp.to_i
 end
 
 puts board_layout
 puts "It's #{player2}'s turn"
-puts 'Please select an available number from the board'
+puts "Please select an available number from the board"
 puts
 second_num = gets.chomp.to_i
 puts
-if second_num <= 9 || second_num >= 1
-  second_num
-elsif second_num == num
-  'You entered a duplicate number'
-else
-  'Invalid entry'
+until board.include?(second_num)
+  puts "invalid move. Please enter a number from 1-9"
+  second_num = gets.chomp.to_i
 end
 
-game_on = true
-
-while game_on
+i = 0
+while i <= 9
   puts board_layout
   number = gets.chomp.to_i
-  first_player = [1, 2, 3].include? number
-  second_player = [4, 5, 6].include? number
-  if first_player || second_player
-    game_on = false
+  if [1, 2, 3].include? number
+    puts "Game Over! \n The winner is #{player1}"
+  elsif [4, 5, 6].include? number
+    puts "Game Over! \n The winner is #{player2}"
   else
-    game_on = true
-    if first_player
-      puts 'Game over!'
-      puts "The winner is #{player1}!"
-      game_on = false
-    elsif second_player
-      puts 'Game over!'
-      puts "The winner is #{player2}!"
-      game_on = false
-    else
-      puts "It's tie! \n Game over"
-    end
+    puts "It's tie! \n Game over"
   end
-  game_on = false
+  i += 1
 end
