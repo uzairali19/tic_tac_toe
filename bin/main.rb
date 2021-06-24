8127cf9fedeb0ea842bae6b5dbb66d064da36c5f
Uzair <uzairali19b@gmail.com>
2021-06-24 20:41:03 +0500

    update moves & position

--- bin/main.rb
+++ bin/main.rb
@@ -1,78 +1,34 @@
-require_relative '../lib/players'
+require_relative "../lib/players"
+require_relative "../lib/board"
 
 puts "Welcome to Ruby's Tic-Tac-Toe!"
 puts
-puts 'Enter Player 1 Name:'
+puts "Enter Player 1 Name:"
 player1 = gets.chomp
 if player1.empty?
-  puts 'Name field cannot be empty'
+  puts "Name field cannot be empty"
   player1 = gets.chomp
 elsif player1.length < 3
-  puts 'Name cannot be less than 3 letters'
+  puts "Name cannot be less than 3 letters"
   player1 = gets.chomp
 elsif player1.to_i.to_s.eql? player1
-  puts 'Only text is allowed'
+  puts "Only text is allowed"
   player1 = gets.chomp
 end
 puts
-puts 'Enter Player 2 Name:'
+puts "Enter Player 2 Name:"
 player2 = gets.chomp
 if player2.empty?
-  puts 'Name field cannot be empty'
+  puts "Name field cannot be empty"
   player2 = gets.chomp
 elsif player2.length < 3
-  puts 'Name cannot be less than 3 letters'
+  puts "Name cannot be less than 3 letters"
   player2 = gets.chomp
 elsif player2.to_i.to_s.eql? player2
-  puts 'Only text is allowed'
+  puts "Only text is allowed"
   player2 = gets.chomp
 end
 puts
 puts "#{player1} will play with X and #{player2} will play with O"
 puts
 puts "Let's start!"
-
-board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-puts board_layout = " +---+---+---+
- | #{board[0]} | #{board[1]} | #{board[2]} |
- +---+---+---+
- | #{board[3]} | #{board[4]} | #{board[5]} |
- +---+---+---+
- | #{board[6]} | #{board[7]} | #{board[8]} |
- +---+---+---+"
-
-puts "It's #{player1}'s turn"
-puts 'Please select an available number from the board'
-puts
-num = gets.chomp.to_i
-puts
-until board.include?(num)
-  puts 'invalid move. Please enter a number from 1-9'
-  num = gets.chomp.to_i
-end
-
-puts board_layout
-puts "It's #{player2}'s turn"
-puts 'Please select an available number from the board'
-puts
-second_num = gets.chomp.to_i
-puts
-until board.include?(second_num)
-  puts 'invalid move. Please enter a number from 1-9'
-  second_num = gets.chomp.to_i
-end
-
-i = 0
-while i <= 9
-  puts board_layout
-  number = gets.chomp.to_i
-  if [1, 2, 3].include? number
-    puts "Game Over! \n The winner is #{player1}"
-  elsif [4, 5, 6].include? number
-    puts "Game Over! \n The winner is #{player2}"
-  else
-    puts "It's tie! \n Game over"
-  end
-  i += 1
-end
--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,48 @@
-puts 'This is board.rb, the main logic file for tic tac toe'
+class Game_Board
+  attr_reader :board
+
+  def initialize
+    @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  end
+
+  def board_layout
+    puts " +---+---+---+
+ | #{board[0]} | #{board[1]} | #{board[2]} |
+ +---+---+---+
+ | #{board[3]} | #{board[4]} | #{board[5]} |
+ +---+---+---+
+ | #{board[6]} | #{board[7]} | #{board[8]} |
+ +---+---+---+"
+  end
+
+  def move(index, user_marker = "X")
+    @board[index] = user_marker
+  end
+
+  def position_taken(index, user_marker)
+    if @board[index] == user_marker
+      puts true
+    elsif @board[index] != user_marker
+      puts false
+    end
+  end
+
+  WINS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ].freeze
+end
+
+display_layout = Game_Board.new
+
+display_layout.board_layout
+index = gets.to_i - 1
+display_layout.move(index, "O")
+display_layout.board_layout
+display_layout.position_taken(index, "O")
