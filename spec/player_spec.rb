b3c44ca681750db74bc1343ec1c12ad30be101a1
Uzair <uzairali19b@gmail.com>
2021-07-20 19:21:37 +0500

    update not_to cases

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -9,5 +9,12 @@ describe Player do
       expect(player1).to eq('Uzair')
       expect(player2).to eq('Saad')
     end
+
+    it 'Checks if the passed values are not correct' do
+      player1 = player.player1
+      player2 = player.player2
+      expect(player1).not_to eq('Dan')
+      expect(player2).not_to eq('Vic')
+    end
   end
 end
--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,9 +5,14 @@ describe 'Game' do
   subject(:g) { GameBoard.new }
 
   describe 'Initialize' do
-    it 'Initializes the game sequence' do
+    it 'Initializes the game sequence matches with original array' do
       board = g.board
       expect(board).to eq([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    end
+
+    it 'Initializes the game sequence and does not matches with original array' do
+      board = g.board
+      expect(board).not_to eq([3, 5, 3, 4, 5, 6, 7, 8, 9])
     end
   end
 
@@ -16,13 +21,15 @@ describe 'Game' do
       input_to_index = g.input_to_index(1)
       expect(input_to_index).to eq(0)
     end
+
     it 'gets user input and checks if input is converted to Integer' do
       input_to_index = g.input_to_index('1')
       expect(input_to_index).to eq(0)
     end
+
     it 'gets user input and checks if input is a number or not' do
       input_to_index = g.input_to_index('This is a long text')
-      expect(input_to_index).to eq(-1)
+      expect(input_to_index).not_to eq(0)
     end
   end
 
@@ -32,13 +39,32 @@ describe 'Game' do
       board = g.board[1]
       expect(board).to eq('X')
     end
+
+    it 'Does not replaces the index variable with user marker' do
+      move = g.move(1, 'abc')
+      board = g.board[1]
+      case board
+      when 'X'
+        expect(board).to eq('X')
+      when 'O'
+        expect(board).to eq('O')
+      else
+        expect(board).not_to eq('X' || 'O')
+      end
+    end
   end
 
   describe 'Position Taken' do
     it 'Checks if postion on board is taken' do
       move = g.move(1, 'O')
       position_taken = g.position_taken?(1)
-      expect(position_taken).to be_truthy if move == g.move(1, 'O')
+      expect(position_taken).to be_truthy if move
+    end
+
+    it 'Checks if postion on board is not taken' do
+      move = g.move(1, 'O')
+      position_taken = g.position_taken?(2)
+      expect(position_taken).not_to be_truthy if move
     end
   end
 
@@ -52,6 +78,18 @@ describe 'Game' do
         expect(valid).to be_truthy
       else
         expect(valid).to be_falsey
+      end
+    end
+
+    it 'Checks if the move is not valid' do
+      index = 1
+      move = g.move(index, 'O')
+      valid = g.valid_move?(index)
+      position_taken = g.position_taken?(2)
+      if index.between?(0, 8) && !position_taken
+        expect(valid).not_to be_truthy
+      else
+        expect(valid).not_to be_falsey
       end
     end
   end
